c96556cae8cf5e2e8dd4ceb73bddde732e9d52ad
Ishwar Jagdale <ishwarrules@gmail.com>
2023-01-06 00:08:10 +0530

    testing scanner

--- src/view/Scanner.jsx
+++ src/view/Scanner.jsx
@@ -43,20 +43,21 @@ class Scanner extends React.Component {
                                     <span className={"font-Poppins font-[500] my-4"}>Scan QR code to know more!</span>
                                 </div>
                                 :
-                                this.state.qr === "ajanta-caves-1234" && <div className={"cursor-pointer w-full"}>
-                                    <img src={this.state.data.image} alt={this.state.data.name} className={"w-full flex-1 h-[150px] border rounded-xl object-cover"} onError={() => FallBackImage}/>
-                                    <div className={"flex flex-col items-start font-Poppins my-2"}>
-                                        <div className={"flex items-center"}>
-                                            <span className={"text-sm font-medium"}>{this.state.data.name}</span>
-                                        </div>
-                                        <p className={"mt-2 text-sm text-gray-700 text-justify text-ellipsis overflow-hidden max-h-[100px]"}>
-                                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                                        </p>
-                                    </div>
-                                    <button className={"w-full transition-all duration-100 ease-in border-2 border-black font-Poppins font-[500] rounded-lg mt-4 active:scale-[1.1!important] active:bg-black active:text-white py-2"}>
-                                        Read More
-                                    </button>
-                                </div>
+                                <span className={"m-auto font-Poppins font-[500] text-sm"}>{this.state.qr}</span>
+                                // this.state.qr === "ajanta-caves-1234" && <div className={"cursor-pointer w-full"}>
+                                //     <img src={this.state.data.image} alt={this.state.data.name} className={"w-full flex-1 h-[150px] border rounded-xl object-cover"} onError={() => FallBackImage}/>
+                                //     <div className={"flex flex-col items-start font-Poppins my-2"}>
+                                //         <div className={"flex items-center"}>
+                                //             <span className={"text-sm font-medium"}>{this.state.data.name}</span>
+                                //         </div>
+                                //         <p className={"mt-2 text-sm text-gray-700 text-justify text-ellipsis overflow-hidden max-h-[100px]"}>
+                                //             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                                //         </p>
+                                //     </div>
+                                //     <button className={"w-full transition-all duration-100 ease-in border-2 border-black font-Poppins font-[500] rounded-lg mt-4 active:scale-[1.1!important] active:bg-black active:text-white py-2"}>
+                                //         Read More
+                                //     </button>
+                                // </div>
                         }
                     </div>
                 </div>
