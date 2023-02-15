81b1f959f5ddaff7ac6ad382c90d2aeff534f46e
Ishwar Jagdale <ishwarrules@gmail.com>
2023-02-15 10:31:48 +0530

    removed userMedia permissions from Scanner

--- src/views/Scanner.jsx
+++ src/views/Scanner.jsx
@@ -1,45 +1,35 @@
 import QrReader from "react-web-qr-reader";
 import {UilMultiply, UilQrcodeScan} from "@iconscout/react-unicons";
 import {useNavigate} from "react-router-dom";
-import {useEffect, useState} from "react";
 
 function Scanner() {
     const history = useNavigate();
 
-    const [permission, setPermission] = useState(false);
-
     const onScan = (r) => {
-        if(r.data.length !== 0) {
+        if (r.data.length !== 0) {
             window.location.href = r.data;
         }
     }
 
-    useEffect(() => {
-        navigator.mediaDevices.getUserMedia({
-            video: true
-        }).then((res) => {
-            setPermission(true)
-        })
-    })
-
     return (
-        <div className={"fixed top-0 left-0 h-full lg:relative flex flex-col overflow-scroll lg:m-2 lg:rounded-xl lg:ml-0 w-full lg:w-1/4 "}>
-            <button onClick={() => history(-1)} className={"lg:hidden z-50 rounded-full p-2 w-fit fixed right-0 top-0 m-4 bg-black"}>
-                <UilMultiply size={'18px'} color={"white"} />
+        <div
+            className={"fixed top-0 left-0 h-full lg:relative flex flex-col overflow-scroll lg:m-2 lg:rounded-xl lg:ml-0 w-full lg:w-1/4 "}>
+            <button onClick={() => history(-1)}
+                    className={"lg:hidden z-50 rounded-full p-2 w-fit fixed right-0 top-0 m-4 bg-black"}>
+                <UilMultiply size={'18px'} color={"white"}/>
             </button>
             <div className={"bg-white"}>
-                {permission &&
-                    <QrReader className={"lg:rounded-xl scanner"} onScan={onScan} onError={(err) => console.log(err)}
-                              delay={500} facingMode={"environment"} showViewFinder={true}/>}
+                <QrReader className={"lg:rounded-xl scanner"} onScan={onScan} onError={(err) => console.log(err)}
+                          delay={500} facingMode={"environment"} showViewFinder={true}/>
             </div>
-            <div className={"rounded-t-2xl flex flex-col justify-center items-center flex-1 z-10 p-4 -mt-10 border-t bg-white"}>
+            <div
+                className={"rounded-t-2xl flex flex-col justify-center items-center flex-1 z-10 p-4 -mt-10 border-t bg-white"}>
                 <div className={"m-auto flex flex-col items-center pb-10"}>
                     <UilQrcodeScan size={'72px'} color={"gray"}/>
-                    {
-                        permission ? <span className={"font-Poppins font-[500] my-4"}>Scan QR code to know more!</span>
-                            :
-                            <span className={"font-Poppins font-[400] my-4"}>Requires permission to access camera</span>
-                    }
+
+                    <span className={"font-Poppins font-[500] my-4"}>Scan QR code to know more!</span>
+
+
                 </div>
             </div>
         </div>
