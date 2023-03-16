ac4c2c74d9f5b480ea0706143c1f13cd15253031
Ishwar Jagdale <ishwarrules@gmail.com>
2023-03-16 19:20:47 +0530

    old about page

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -24,23 +24,23 @@ function Navigation() {
             <div onClick={() => setMenuActive(!menuActive)} className={`${menuActive ? 'hidden': ''} bg-[rgba(0,0,0,0.2)] lg:bg-transparent flex flex-col justify-end z-50 lg:flex fixed lg:relative bottom-0 left-0 w-full h-full fixed lg:relative lg:w-fit lg:h-fit`}>
                 <div className={`flex flex-col lg:flex-row lg:items-center items-start border-t-2 h-fit bg-white lg:border-0 w-full lg:w-fit text-sm font-Poppins font-[500] rounded-t-2xl`}>
                     <div onClick={() => setMenuActive(!menuActive)} className={"w-[50px] h-[4px] mx-auto bg-black rounded-xl lg:hidden m-2 mb-4"} />
-                    <Link to={"/"} onClick={() => setMenuActive(!menuActive)} className={"flex items-center hover:bg-[#f1f1f1] rounded-xl p-4"}>
+                    <Link to={"/"} onClick={() => setMenuActive(!menuActive)} className={"flex w-full lg:w-fit items-center hover:bg-[#f1f1f1] rounded-xl p-4"}>
                         <div className={"mx-2 lg:hidden"}><UilEstate/></div>
                         <span className={"mx-2"}>Home</span>
                     </Link>
-                    <Link to={"/nearby"} onClick={() => setMenuActive(!menuActive)} className={"flex items-center hover:bg-[#f1f1f1] rounded-xl p-4"}>
+                    <Link to={"/nearby"} onClick={() => setMenuActive(!menuActive)} className={"flex w-full lg:w-fit items-center hover:bg-[#f1f1f1] rounded-xl p-4"}>
                         <div className={"mx-2 lg:hidden"}><UilLocationPoint/></div>
                         <span className={"mx-2"}>Nearby</span>
                     </Link>
-                    <Link to={"/visited-places"} onClick={() => setMenuActive(!menuActive)} className={"flex items-center hover:bg-[#f1f1f1] rounded-xl p-4"}>
+                    <Link to={"/visited-places"} onClick={() => setMenuActive(!menuActive)} className={"flex w-full lg:w-fit items-center hover:bg-[#f1f1f1] rounded-xl p-4"}>
                         <div className={"mx-2 lg:hidden"}><UilHistoryAlt/></div>
                         <span className={"mx-2"}>Visited Places</span>
                     </Link>
-                    <Link to={"/saved"} onClick={() => setMenuActive(!menuActive)} className={"flex items-center hover:bg-[#f1f1f1] rounded-xl p-4"}>
+                    <Link to={"/saved"} onClick={() => setMenuActive(!menuActive)} className={"flex w-full lg:w-fit items-center hover:bg-[#f1f1f1] rounded-xl p-4"}>
                         <div className={"mx-2 lg:hidden"}><UilBookmark/></div>
                         <span className={"mx-2"}>Saved Places</span>
                     </Link>
-                    <Link to={"/about"} onClick={() => setMenuActive(!menuActive)} className={"flex items-center hover:bg-[#f1f1f1] rounded-xl p-4"}>
+                    <Link to={"/about"} onClick={() => setMenuActive(!menuActive)} className={"flex w-full lg:w-fit items-center hover:bg-[#f1f1f1] rounded-xl p-4"}>
                         <div className={"mx-2 lg:hidden"}><UilAdjust/></div>
                         <span  className={"mx-2"}>About</span>
                     </Link>
