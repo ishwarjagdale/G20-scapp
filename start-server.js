37506f31eba3fded5ce4cedbb6f31f0ebb7a3772
Ishwar Jagdale <ishwarrules@gmail.com>
2023-03-20 08:28:58 +0530

    fix express couldn't find url on reload

--- start-server.js
+++ start-server.js
@@ -3,8 +3,12 @@ const path = require('path');
 const app = express();
 
 app.use("/smart-scan", express.static(path.join(__dirname, 'build')));
+//
+// app.get('/smart-scan', function (req, res) {
+//     res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
 
-app.get('/smart-scan', function (req, res) {
+app.get('/smart-scan/*', function (req, res) {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
